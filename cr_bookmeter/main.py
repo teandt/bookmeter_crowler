c9ab5c23a0f457e04d9ccd9db1c1032d1bab0b81
tea <teandt@gmail.com>
2025-08-14 20:31:39 +0900

    ファイル出力の設定確認

--- cr_bookmeter/main.py
+++ cr_bookmeter/main.py
@@ -2,6 +2,10 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from cr_bookmeter.spiders.bookmeter import BookmeterSpider
 
-process = CrawlerProcess(get_project_settings())
+settings = get_project_settings()
+settings.set('FEED_FORMAT', 'json')
+settings.set('FEED_URI', 'output_read2.json')
+
+process = CrawlerProcess(settings)
 process.crawl(BookmeterSpider)
 process.start()
