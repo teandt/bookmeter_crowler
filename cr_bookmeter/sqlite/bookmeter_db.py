f86b3e6d93c29bc959ae5192d249fafbca73676d
tea <teandt@gmail.com>
2025-08-16 19:24:40 +0900

    リスト用のテーブル構造を設定

--- cr_bookmeter/sqlite/bookmeter_db.py
+++ cr_bookmeter/sqlite/bookmeter_db.py
@@ -6,14 +6,28 @@ from sqlalchemy.orm import sessionmaker
 engine = sqlalchemy.create_engine('sqlite:///bookmeter.db', echo=True)
 Base = declarative_base()
 
-class ReadBooks(Base):
-    __tablename__ = 'read_books'
+# 積読本と読んだ本リストの共通項目
+class BookList:
     book_id = Column(String, primary_key=True)
     title = Column(String)
     authors = Column(String)
     date = Column(String)
     url = Column(String)
 
+# 読んだ本リスト
+class ReadBooks(BookList, Base):
+    __tablename__ = 'read_books'
+
     def __repr__(self):
         return f"<ReadBooks(book_id='{self.book_id}', title='{self.title}', authors='{self.authors}', date='{self.date}', url='{self.url}')>"
 
+# 積読本リスト
+class StackedBooks(BookList, Base):
+    __tablename__ = 'stacked_books'
+
+    def __repr__(self):
+        return f"<StackedBooks(book_id='{self.book_id}', title='{self.title}', authors='{self.authors}', date='{self.date}', url='{self.url}')>"
+
+if __name__ == '__main__':
+    # テーブルを作成
+    Base.metadata.create_all(engine)
