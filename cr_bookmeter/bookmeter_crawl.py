64660f8436fd7798b82133b741ad655e276966a5
tea <teandt@gmail.com>
2025-08-16 19:12:10 +0900

    ログ形式をscrapyに合わせるかたちで設定

--- cr_bookmeter/bookmeter_crawl.py
+++ cr_bookmeter/bookmeter_crawl.py
@@ -6,9 +6,15 @@ from cr_bookmeter.spiders.bookmeter_read import BookmeterReadSpider
 from cr_bookmeter.spiders.bookmeter_stacked import BookmeterStackedSpider
 from twisted.internet.error import ReactorNotRestartable
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(levelname)s : %(name)s - %(message)s')
-logger = logging.getLogger(__name__)
+# logのフォーマットはscrapyに合わせる形で指定しておく
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger('bookmeter_crawl')
 
+# コマンドライン引数の処理
 parser = argparse.ArgumentParser(
                     prog='Bookmeter Crawler',
                     description='読書メーターの未読／既読リスト取得ツール',
@@ -21,32 +27,34 @@ args = parser.parse_args()
 if not any(vars(args).values()):
     parser.error("少なくとも1つのオプション (-st, -rd, -vb) を指定してください。")
 
-# CrawlerProcessは一度だけ初期化し、実行したいスパイダーをすべて追加してから
-# 最後に一度だけstart()を呼び出します。
-settings = get_project_settings()
-process = CrawlerProcess(settings)
+if __name__ == "__main__":
+    # CrawlerProcessは一度だけ初期化し、実行したいスパイダーをすべて追加してから
+    # 最後に一度だけstart()を呼び出します。
+    settings = get_project_settings()
+    process = CrawlerProcess(settings, install_root_handler=False)
 
-crawled_something = False
-if args.stacked:
-    logger.info("積読本リストの取得をキューに追加します。")
-    process.crawl(BookmeterStackedSpider)
-    crawled_something = True
+    crawled_something = False
+    # 積読本リストの取得を行う場合
+    if args.stacked:
+        logger.info("積読本リストの取得をキューに追加します。")
+        process.crawl(BookmeterStackedSpider)
+        crawled_something = True
 
-if args.read:
-    logger.info("読んだ本リストの取得をキューに追加します。")
-    process.crawl(BookmeterReadSpider)
-    crawled_something = True
+    # 読んだ本リストの取得を行う場合
+    if args.read:
+        logger.info("読んだ本リストの取得をキューに追加します。")
+        process.crawl(BookmeterReadSpider)
+        crawled_something = True
 
-# キューにスパイダーが追加されている場合のみ、クローリングを開始します。
-if crawled_something:
-    try:
-        logger.info("クローリング処理を開始します...")
-        process.start()
-        logger.info("クローリング処理が正常に完了しました。")
-    except ReactorNotRestartable:
-        logger.error("Reactorは再起動できません。スクリプトの構造を確認してください。")
-    except Exception as e:
-        logger.error(f"クローリング中に予期せぬエラーが発生しました: {e}", exc_info=True)
+    # 積読本リスト／読んだ本リストで書籍詳細を取得していない本がある場合に詳細を取得する処理
 
-# 積読本リスト／読んだ本リストで書籍詳細を取得していない本がある場合に詳細を取得する処理
-
+    # キューにスパイダーが追加されている場合のみ、クローリングを開始します。
+    if crawled_something:
+        try:
+            logger.info("クローリング処理を開始します...")
+            process.start()
+            logger.info("クローリング処理が正常に完了しました。")
+        except ReactorNotRestartable:
+            logger.error("Reactorは再起動できません。スクリプトの構造を確認してください。")
+        except Exception as e:
+            logger.error(f"クローリング中に予期せぬエラーが発生しました: {e}", exc_info=True)
